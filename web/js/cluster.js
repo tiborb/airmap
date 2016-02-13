78fdc48a8cbcae74e03ec93009cc7964f27d4e4f
tiborb <tibor.barna85@gmail.com>
2016-02-13 22:28:47 +0100

    Display geojson on the map

--- web/js/cluster.js
+++ web/js/cluster.js
@@ -1,4 +1,29 @@
-var app = angular.module('demoapp', ['leaflet-directive']);
-app.controller('BasicFirstController', ['$scope', function($scope) {
-  // Nothing here!}
-}]);
+var app = angular.module("demoapp", ["leaflet-directive"]);
+app.controller("GeoJSONController", [ '$scope', '$http', 'leafletData', function($scope, $http, leafletData) {
+  angular.extend($scope, {
+      stuttgart: {
+          lat: 48.74,
+          lng: 9.21,
+          zoom: 10
+      },
+      defaults: {
+          scrollWheelZoom: true
+      }
+  });
+  // Get the countries geojson data from a JSON
+  $http.get("json/dustipos.json").success(function(data, status) {
+      angular.extend($scope, {
+          geojson: {
+              data: data,
+              style: {
+                  fillColor: "green",
+                  weight: 2,
+                  opacity: 1,
+                  color: 'white',
+                  dashArray: '3',
+                  fillOpacity: 0.7
+              }
+          }
+      });
+  });
+} ]);
