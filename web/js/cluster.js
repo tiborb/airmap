5d213438586aade8959039f7f5d4bfc42834e305
tiborb <tibor.barna85@gmail.com>
2016-02-15 23:52:57 +0100

    Added geojson points clustering

--- web/js/cluster.js
+++ web/js/cluster.js
@@ -1,29 +1,60 @@
-var app = angular.module("demoapp", ["ui-leaflet"]);
-app.controller("GeoJSONController", [ '$scope', '$http', 'leafletData', function($scope, $http, leafletData) {
+var app = angular.module('demoapp', ['ui-leaflet']);
+app.controller('GeoJSONController', ['$scope', '$http', 'leafletData', function($scope, $http, leafletData) {
   angular.extend($scope, {
-      stuttgart: {
-          lat: 48.74,
-          lng: 9.21,
-          zoom: 10
-      },
-      defaults: {
-          scrollWheelZoom: true
-      }
+    stuttgart: {
+      lat: 48.74,
+      lng: 9.21,
+      zoom: 10
+    },
+    defaults: {
+      scrollWheelZoom: true
+    }
   });
+
+  L.GeoJSON = L.GeoJSON.extend({
+       addTo: function(map) {
+           var self = this;
+           map.addLayer(this.markers);
+           var parentRemove = L.GeoJSON.prototype.onRemove;
+           L.GeoJSON.prototype.onRemove = function (map) {
+               self.markers.removeLayer(self);
+               delete self.markers;
+               parentRemove(map);
+           };
+           return this;
+       }
+   });
+
+   L.geoJson = function (geojson, options) {
+       var geoJSON = new L.GeoJSON(geojson, options);
+       var markers = new L.MarkerClusterGroup({
+           spiderfyOnMaxZoom: false,
+           showCoverageOnHover: true,
+           zoomToBoundsOnClick: true,
+           disableClusteringAtZoom: 18
+       });
+       markers.setGeoJSON = function(data) {
+           geoJSON.setGeoJSON(data);
+       };
+       markers.addLayer(geoJSON);
+       geoJSON.markers = markers;
+       return  markers;
+   };
+
   // Get the countries geojson data from a JSON
-  $http.get("json/dustipos.json").success(function(data, status) {
-      angular.extend($scope, {
-          geojson: {
-              data: data,
-              style: {
-                  fillColor: "green",
-                  weight: 2,
-                  opacity: 1,
-                  color: 'white',
-                  dashArray: '3',
-                  fillOpacity: 0.7
-              }
-          }
-      });
+  $http.get('json/dustipos.json').success(function(data, status) {
+    angular.extend($scope, {
+      geojson: {
+        data: data,
+        style: {
+          fillColor: 'green',
+          weight: 2,
+          opacity: 1,
+          color: 'white',
+          dashArray: '3',
+          fillOpacity: 0.7
+        }
+      }
+    });
   });
-} ]);
+}]);
